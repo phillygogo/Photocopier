5351fdb6b7675b5918e5e8d142528244d1e7256d
Phillip Rooney <phillip.rooney@orbitremit.com>
2018-11-16 17:50:45 +1300

    Adding the start of Facebook authentication

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -13,7 +13,47 @@ class PhotoController extends Controller
      */
     public function index()
     {
-        return 'Phillip in controller';
+        return view('/photo/homepage');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate()
+    {
+        $fb = new \Facebook\Facebook([
+            'app_id' => env('client_id'),
+            'app_secret' => env('client_secret'),
+            'default_graph_version' => 'v2.2',
+        ]);
+
+        $helper = $fb->getCanvasHelper();
+
+        try {
+            $accessToken = $helper->getAccessToken();
+        } catch (Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        if (!isset($accessToken)) {
+            echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+            exit;
+        }
+
+        // Logged in
+        echo '<h3>Signed Request</h3>';
+        var_dump($helper->getSignedRequest());
+
+        echo '<h3>Access Token</h3>';
+        var_dump($accessToken->getValue());
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,7 @@
 | by your application. Just tell Laravel the URIs it should respond
 | to using a Closure or controller method. Build something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/photo', 'PhotoController@index');
+Route::get('/', 'PhotoController@index');
+Route::get('/authenticate', 'PhotoController@authenticate');
